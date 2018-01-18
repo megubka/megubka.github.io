5f8e5c3a0ae90b3f967a3cf9326f4bc899c24c54
Alimzhan Aigarayev <alimzhan.aigarayev@gmail.com>
2018-01-18 14:30:44 +0100

    final commit

--- matigas/js/common.js
+++ matigas/js/common.js
@@ -5,11 +5,32 @@ $(document).ready(function() {
 	heightDetect();
 	$(window).resize(function() {
 		heightDetect();
-	})
+	});
+
+	//E-mail Ajax Send
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Спасибо! В кратчайшие сроки мы свяжемся с вами.");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+	$("header a").mPageScroll2id();
 	
 });
 
+//PreLoader 
 $(window).load(function() { 
 	$(".loader_inner").fadeOut(); 
 	$(".loader").delay(400).fadeOut("slow"); 
 });
+
